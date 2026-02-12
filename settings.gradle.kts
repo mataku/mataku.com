d5cd4de2bee6e5db085cfa5215086eb617c45990
Takuma Homma <nagomimatcha@gmail.com>
2026-02-12 23:11:26 +0900

    Cleanup worker

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "blog"
-include("generator", "worker")
+include("generator")
--- worker/entry.js
+++
@@ -1,7 +0,0 @@
-import blog from './build/compileSync/js/main/productionExecutable/kotlin/blog-worker.js';
-
-export default {
-  fetch(request, env, ctx) {
-    return blog.fetch(request, env);
-  }
-};
