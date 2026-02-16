ef08724a3265229b49d257cb5a9a3501ff775204
Takuma Homma <nagomimatcha@gmail.com>
2026-02-17 01:26:35 +0900

    Wasm

--- worker/build.gradle.kts
+++ worker/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.targets.js.ir.Executable
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
 }
@@ -7,15 +10,54 @@ repositories {
 }
 
 kotlin {
-    js {
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
         nodejs()
         binaries.executable()
-        compilations.all {
-            compileTaskProvider.configure {
-                compilerOptions {
-                    sourceMap.set(false)
-                    sourceMapEmbedSources.set(org.jetbrains.kotlin.gradle.dsl.JsSourceMapEmbedMode.SOURCE_MAP_SOURCE_CONTENT_NEVER)
+
+        compilations.configureEach {
+            binaries.withType<Executable>().configureEach {
+                linkTask.configure {
+                    val moduleName = linkTask.flatMap { it.compilerOptions.moduleName }
+                    val fileName = moduleName.map { "$it.uninstantiated.mjs" }
+                    val mjsFile = linkTask.flatMap { it.destinationDirectory.file(fileName.get()) }
+
+                    doLast {
+                        val module = moduleName.get()
+                        val file = mjsFile.get().asFile
+                        val text = file.readText()
+                        val newText = text
+                            .replace(
+                                "if (!isNodeJs && !isDeno && !isStandaloneJsVM && !isBrowser) {\n      throw \"Supported JS engine not detected\";\n    }",
+                                ""
+                            )
+                            .replace(
+                                Regex("""if \(isNodeJs\) \{.+?\n      \}""", RegexOption.DOT_MATCHES_ALL),
+                                ""
+                            )
+                            .replace(
+                                "if (isBrowser) {",
+                                """
+                                |const isCloudflareWorker = typeof navigator !== 'undefined' && navigator.userAgent.includes("Cloudflare-Workers");
+                                |      if (isCloudflareWorker) {
+                                |        const { default: wasmModule } = await import('./$module.wasm');
+                                |        wasmInstance = (await WebAssembly.instantiate(wasmModule, importObject));
+                                |      }
+                                |
+                                |      else if (isBrowser) {
+                                """.trimMargin()
+                            )
+                        file.writeText(newText)
+                    }
                 }
+            }
+        }
+    }
+
+    sourceSets {
+        wasmJsMain {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-browser:0.3")
             }
         }
     }
